84856057c03ed73ad856afd78b8ffd2378c40249
Luca(MingCong) Zhou <zhoumingcong6366@gmail.com>
2020-12-25 00:55:20 -0500

    modify / route to render index.html

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 
 def main():
